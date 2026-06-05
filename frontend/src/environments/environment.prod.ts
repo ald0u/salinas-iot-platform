3471dfdd651b9fc46c9da6edf8e002d7050f4350
ald0u <aldoulaguent@gmail.com>
2026-06-05 00:03:23 -0600

    chore(frontend): build de produccion con nginx apuntando al backend en la nube

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://salinas-iot-backend.ylbuuv.easypanel.host/api/v1',
+  wsUrl: 'https://salinas-iot-backend.ylbuuv.easypanel.host',
+};
